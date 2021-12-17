088f20735f2ce97e441a470c36c74c2580594d93
Alliana Dela Peña <32075424+allianad@users.noreply.github.com>
2021-12-17 11:50:45 -0700

    Add files via upload

---
+++ Project/Manager Pages/editMovieShowingF.php
@@ -0,0 +1,28 @@
+<html>
+
+<header>
+	
+	<link rel="stylesheet" type="text/css">
+
+	<title></title>
+
+</header>
+
+<body>
+    <nav class ="navbar" id ="navbar">
+        <p1> Website </p1>
+        <ul>
+        <li> <a href="managerAccountInfoF.php"> Account Info </a> </li>
+            <li> <a href="editMovieF.php"> Edit Movies </a> </li>
+            <li> <a href="editMovieShowingF.php"> Edit Movie Showings </a> </li>
+    
+        </ul>
+    </nav>
+	<h1>Edit Movie Showings</h1>
+	<div>
+		<ul class="info">
+            <li>Here managers will be able to add/remove movie showings </li>
+        </ul>
+	</div>
+</body>
+</html>
